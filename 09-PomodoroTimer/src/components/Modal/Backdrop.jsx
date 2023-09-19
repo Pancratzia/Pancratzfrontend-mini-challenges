8eb6c1c0c95597c9cbddc34ebd338fed4a29b454
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-09-19 11:00:03 -0400

    Starting modal construction

--- 09-PomodoroTimer/src/components/Modal/Backdrop.jsx
+++ 09-PomodoroTimer/src/components/Modal/Backdrop.jsx
@@ -3,8 +3,19 @@ import styled from 'styled-components';
 
 const Backdrop = () => {
   return (
-    <div>Backdrop</div>
+    
+    <KBackdrop></KBackdrop>
+    
   )
 }
 
-export default Backdrop
+export default Backdrop;
+
+const KBackdrop = styled.div`
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 100;
+`;
--- 09-PomodoroTimer/src/components/Modal/MadalContainer.jsx
+++ 09-PomodoroTimer/src/components/Modal/MadalContainer.jsx
@@ -3,8 +3,29 @@ import styled from 'styled-components';
 
 const MadalContainer = () => {
   return (
-    <div>MadalContainer</div>
+    <Container>
+      <ModalContent>
+        <ModalHeader>
+          <ModalTitle>Modal Title</ModalTitle>
+          <ModalCloseButton>X</ModalCloseButton>
+        </ModalHeader>
+        <ModalBody></ModalBody>
+      </ModalContent>
+    </Container>
   )
 }
 
-export default MadalContainer
+export default MadalContainer;
+
+
+const Conainter = styled.div``;
+
+const ModalContent = styled.div``;
+
+const ModalHeader = styled.div``;
+
+const ModalTitle = styled.div``;
+
+const ModalCloseButton = styled.div``;
+
+const ModalBody = styled.div``;
