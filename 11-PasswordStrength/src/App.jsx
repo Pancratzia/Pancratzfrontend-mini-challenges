c5c6111c841ae5f8e9f9e00aeb7d5681aa6cbf8a
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-09-21 12:53:50 -0400

    Meter created with styles. Functionalities to be done

--- 11-PasswordStrength/src/App.jsx
+++ 11-PasswordStrength/src/App.jsx
@@ -9,12 +9,10 @@ function App() {
       <h1 className="text-center mt-3 fw-bold text-uppercase">Password Strength Checker</h1>
       <h2 className="text-center mb-5">by Pancratzia</h2>
 
-      <div className="form-group">
-        <input type="password" className="form-control" placeholder="Password..." />
-
-        <PasswordStrengthMeter />
-
+      <div className="form-group mb-1">
+        <input type="password" className="form-control shadow-none" placeholder="Password..." />
       </div>
+        <PasswordStrengthMeter />
     </div>
    </div>
   )
--- 11-PasswordStrength/src/components/PasswordStrengthMeter.jsx
+++ 11-PasswordStrength/src/components/PasswordStrengthMeter.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 
 const PasswordStrengthMeter = () => {
+
+    const changePasswordColor = () => ({
+        background: 'green',
+        width: '70%',
+        height: '7px',
+    })
+
   return (
-    <div>
-        <p>Meter</p>
-    </div>
+    <>
+        <div className="progress" style={{height: '7px'}}>
+            <div className="progress-bar" style={changePasswordColor()}>
+
+            </div>
+        </div>
+    </>
   )
 }
 
