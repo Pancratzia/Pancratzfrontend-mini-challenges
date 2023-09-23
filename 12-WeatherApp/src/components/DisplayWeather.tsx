e91b857f32e4f4126e8b10a9f94508a419b5b90e
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-09-23 14:09:36 -0400

    UI created

--- 12-WeatherApp/src/components/DisplayWeather.tsx
+++ 12-WeatherApp/src/components/DisplayWeather.tsx
@@ -1,18 +1,51 @@
-import  { MainWrapper } from './styles.module';
-import { AiOutlineSearch } from 'react-icons/ai';
+import { MainWrapper } from "./styles.module";
+import { AiOutlineSearch } from "react-icons/ai";
+import { WiHumidity } from "react-icons/wi";
+import { SiWindicss } from "react-icons/si";
+import { BsFillSunFill, BsCloudyFill, BsFillCloudRainFill, BsCloudFog2Fill  } from "react-icons/bs";
+import {RiLoaderFill} from "react-icons/ri";
+import { TiWeatherPartlySunny } from "react-icons/ti";
+
 const DisplayWeather = () => {
   return (
     <MainWrapper>
-        <div className="container">
-            <div className="searchArea">
-                <input type="text" placeholder="Enter a city..."/>
-            </div>
-            <div className="searchCircle">
-                <AiOutlineSearch className="searchIcon" />
-            </div>
-        </div>
-    </MainWrapper>
-  )
-}
+      <div className="container">
+        <div className="searchArea">
+          <input type="text" placeholder="Enter a city..." />
 
-export default DisplayWeather
+          <div className="searchCircle">
+            <AiOutlineSearch className="searchIcon" />
+          </div>
+        </div>
+
+        <div className="weatherArea">
+          <h1>Barquisimeto</h1>
+          <span>VE</span>
+          <div className="icon">icon</div>
+          <h1>18º</h1>
+          <h2>Cloudy</h2>
+        </div>
+
+        <div className="bottomInfoArea">
+          <div className="humidityLevel">
+            <WiHumidity className="windIcon" />
+            <div className="humidInfo">
+              <h1>60%</h1>
+              <p>Humidity</p>
+            </div>
+          </div>
+
+          <div className="wind">
+            <SiWindicss className="windIcon" />
+            <div className="humidInfo">
+              <h1>2.35km/h</h1>
+              <p>Wind Speed</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </MainWrapper>
+  );
+};
+
+export default DisplayWeather;
