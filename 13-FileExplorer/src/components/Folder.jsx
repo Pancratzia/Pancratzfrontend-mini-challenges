3e8034cd803677f08dd058d14463a217f1d82ad9
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-09-25 17:16:11 -0400

    Creating insert file and folder UI before starting with the logic and functionality

--- 13-FileExplorer/src/components/Folder.jsx
+++ 13-FileExplorer/src/components/Folder.jsx
@@ -2,6 +2,19 @@ import { useState } from "react";
 
 const Folder = ({ explorer }) => {
   const [expand, setExpand] = useState(false);
+  const [showInput, setShowInput] = useState({
+    visible: false,
+    isFolder: null,
+  });
+
+  const handleNewFolder = (e, isFolder) => {
+    e.stopPropagation();
+    setExpand(true);
+    setShowInput({
+      visible: true,
+      isFolder
+    })
+  };
 
   if (explorer.isFolder) {
     return (
@@ -12,8 +25,25 @@ const Folder = ({ explorer }) => {
           onClick={() => setExpand(!expand)}
         >
           <span>📁 {explorer.name}</span>
+
+          <div>
+            <button onClick={(e) => handleNewFolder(e, true)}>➕📁</button>
+            <button onClick={(e) => handleNewFolder(e, false)}>➕📃</button>
+          </div>
         </div>
         <div style={{ display: expand ? "block" : "none", marginLeft: "25px" }}>
+          {showInput.visible && (
+            <div className="inputContainer">
+              <span>{showInput.isFolder ? "📁" : "📃"}</span>
+              <input
+               className="inputContainer__input"
+               type="text"
+               autoFocus
+               onBlur={()=>setShowInput({...showInput, visible: false})}
+               />
+            </div>
+          )}
+
           {explorer.items.map((item) => {
             return <Folder key={item.id} explorer={item} />;
           })}
