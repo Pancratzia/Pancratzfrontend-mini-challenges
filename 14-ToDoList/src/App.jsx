1f6ac23f7587814a2b9d847a381165e41fc794be
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-09-26 15:59:49 -0400

    Creating custom Hooks and using useReducer

--- 14-ToDoList/src/App.jsx
+++ 14-ToDoList/src/App.jsx
@@ -1,7 +1,19 @@
 import "./App.css";
 import TodoAdd from "./components/TodoAdd";
 import TodoList from "./components/TodoList";
+import { useTodo } from "./hooks/useTodo";
 function App() {
+  
+  const {
+    todos,
+    todosCount,
+    pendingTodosCount,
+    handleNewTodo,
+    handleDeleteTodo,
+    handleCompleteTodo,
+    handleUpdateTodo,
+  } = useTodo();
+
   return (
     <>
       <div className="card-to-do">
